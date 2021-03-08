c52a5b82117fcd7dc8519bbdde4ff8e4b4b40dbc
David Ammeraal <david.ammeraal@gmail.com>
2021-03-08 22:14:55 +0100

    First commit

---
+++ src/stories/useMovable.stories.tsx
@@ -0,0 +1,213 @@
+import React, {
+  CSSProperties,
+  useCallback,
+  useContext,
+  useRef,
+  useState,
+} from "react";
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Story, Meta } from "@storybook/react/types-6-0";
+
+import { MoveData, useMovable, UseMovableProps } from "../useMovable";
+import { MovableConnector, withMouse } from "../connector";
+import { emptyPosition2D, Position2D } from "../util";
+
+export default {
+  title: "Example/useMovable",
+} as Meta;
+
+interface MovableProps extends React.HTMLAttributes<HTMLDivElement> {
+  delta?: Position2D;
+  position?: Position2D;
+}
+
+const StoryContext = React.createContext({
+  boundsRef: undefined as React.RefObject<HTMLElement> | undefined,
+});
+
+const Movable = React.forwardRef<HTMLDivElement, MovableProps>(
+  ({ delta, position = emptyPosition2D(), style, ...props }, ref) => {
+    const styleProp: CSSProperties = {
+      ...style,
+      position: "absolute",
+      left: position.x,
+      top: position.y,
+      padding: "2em",
+      border: "3px solid black",
+      boxSizing: "border-box",
+      userSelect: "none",
+      ...(delta
+        ? { transform: `translate3d(${delta.x}px, ${delta.y}px, 0)` }
+        : {}),
+    };
+    return <div {...props} style={styleProp} ref={ref} />;
+  }
+);
+
+const MovableHandle = React.forwardRef<HTMLDivElement, MovableProps>(
+  ({ delta, style, ...rest }, ref) => {
+    const styleProp: CSSProperties = {
+      ...style,
+      position: "absolute",
+      backgroundColor: "red",
+      borderRadius: "50%",
+      width: "1em",
+      height: "1em",
+      top: ".5em",
+      left: ".5em",
+    };
+    return <div {...rest} style={styleProp} ref={ref} />;
+  }
+);
+
+interface MovableStoryTemplateArgs extends UseMovableProps {
+  withHandle?: boolean;
+}
+
+const Template: Story<MovableStoryTemplateArgs> = (args) => {
+  const { withHandle, bounds } = args;
+  const { boundsRef } = useContext(StoryContext);
+
+  const sizeRef = useRef<HTMLDivElement>(null);
+  const [state, setState] = useState({
+    moving: false,
+    delta: undefined as Position2D | undefined,
+    position: undefined as Position2D | undefined,
+  });
+
+  const handleMoveStart = useCallback(() => {
+    setState((s) => ({ ...s, moving: true }));
+  }, []);
+
+  const handleEndMoveEnd = useCallback((e: MoveData) => {
+    setState((s) => {
+      const pos = s.position || emptyPosition2D();
+      const newPosition = { x: pos.x + e.delta.x, y: pos.y + e.delta.y };
+      return {
+        ...s,
+        moving: false,
+        position: newPosition,
+        delta: undefined,
+      };
+    });
+  }, []);
+
+  const handleMove = useCallback((e: MoveData) => {
+    setState((s) => ({ ...s, delta: e.delta }));
+  }, []);
+
+  const ref = useMovable({
+    ...args,
+    bounds: boundsRef || bounds,
+    onMoveStart: handleMoveStart,
+    onMove: handleMove,
+    sizeRef,
+    onMoveEnd: handleEndMoveEnd,
+  });
+
+  return (
+    <div
+      data-testid="parent"
+      style={{
+        position: "relative",
+        marginTop: 10,
+        marginLeft: 10,
+        width: 300,
+        height: 300,
+        backgroundColor: "lightgrey",
+      }}
+    >
+      <Movable
+        ref={withHandle ? sizeRef : ref}
+        delta={state.delta}
+        position={state.position}
+        tabIndex={0}
+        data-testid="movable"
+        data-moving={state.moving}
+      >
+        {withHandle && <MovableHandle ref={ref} data-testid="handle" />}
+        Drag me
+      </Movable>
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
+export const Unbounded = Template.bind({});
+Unbounded.args = {
+  unbounded: true,
+};
+export const Bounded = Template.bind({});
+Bounded.decorators = [
+  (RenderedStory) => {
+    const ref = useRef<HTMLDivElement>(null);
+    return (
+      <StoryContext.Provider value={{ boundsRef: ref }}>
+        <div
+          ref={ref}
+          style={{
+            padding: "3em",
+            display: "inline-block",
+            backgroundColor: "darkgrey",
+          }}
+        >
+          <RenderedStory />
+        </div>
+      </StoryContext.Provider>
+    );
+  },
+];
+
+export const WithHandle = Template.bind({});
+WithHandle.args = {
+  withHandle: true,
+};
+
+const withKeyboard = (connector?: MovableConnector): MovableConnector => {
+  const listeners = {} as { [key: string]: (...args: any) => void };
+
+  return {
+    connect: (el, actions) => {
+      connector?.connect(el, actions);
+      listeners.move = (e: KeyboardEvent) => {
+        if (document.activeElement !== el) return;
+        e.preventDefault();
+        actions.moveStart();
+        switch (e.key) {
+          case "ArrowUp":
+            actions.move({ y: -10 });
+            break;
+          case "ArrowRight":
+            actions.move({ x: 10 });
+            break;
+          case "ArrowDown":
+            actions.move({ y: 10 });
+            break;
+          case "ArrowLeft":
+            actions.move({ x: -10 });
+            break;
+          default:
+        }
+        actions.moveEnd();
+      };
+      window.addEventListener("keydown", listeners.move);
+    },
+    disconnect: (el) => {
+      connector?.disconnect(el);
+      el.addEventListener("focus", listeners.moveStart);
+      window.addEventListener("keydown", listeners.move);
+      el.addEventListener("blur", listeners.moveEnd);
+    },
+  };
+};
+
+export const CustomConnector = Template.bind({});
+CustomConnector.args = {
+  connector: withKeyboard(),
+};
+
+export const ComposedConnector = Template.bind({});
+ComposedConnector.args = {
+  connector: withKeyboard(withMouse()),
+};
---
+++ src/useMovable.ts
@@ -0,0 +1,329 @@
+import React, { useRef, useCallback, useEffect } from "react";
+import { MovableConnector, withMouse, withTouch } from "./connector";
+import {
+  BoundingRect,
+  Position2D,
+  Rect,
+  emptyBoundingRect,
+  toBoundingRect,
+  isBoundingRect,
+  emptyPosition2D,
+} from "./util";
+
+export interface UseMovableProps {
+  /**
+   * Boundary in which the element can be dragged
+   */
+  bounds?:
+    | (() => BoundingRect | undefined)
+    | BoundingRect
+    | React.RefObject<HTMLElement | undefined>;
+  /**
+   * Setting this to false will make the movable object unbounded, it can then be moved outside of its parent or bounding element.
+   */
+  unbounded?: boolean;
+  /**
+   * The element which represents the size of the element being dragged around.
+   */
+  sizeRef?: React.RefObject<HTMLElement>;
+  /**
+   * Called when the element is being moved
+   */
+  onMove?: (data: MoveData) => void;
+  /**
+   * Called when the elements starts moving
+   */
+  onMoveStart?: (data: MoveData) => void;
+  /**
+   * Called when element stops moving
+   */
+  onMoveEnd?: (data: MoveData) => void;
+  /**
+   * Connector that connects hook to DOM or something else.
+   */
+  connector?: MovableConnector;
+  /**
+   * Measuring method, defaults to get [getBoundingClientRect()](https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect)
+   */
+  measure?: (el: HTMLElement) => BoundingRect;
+}
+
+export interface MoveData {
+  /**
+   * The original position from which we started moving.
+   */
+  origin: Position2D;
+  /**
+   * The moved bounding rect.
+   */
+  rect: BoundingRect;
+  /**
+   * The difference in pixels from the origin of the current position.
+   */
+  delta: Position2D;
+}
+
+/**
+ * Calculates the current delta relative to the origin.
+ * @param origin The original event from which we started moving.
+ * @param e The current event which we'll compare to the origin.
+ * @param bounds The bounds in which the element is allowed to move.
+ * @param size The size of the element. We need this so that the element won't overflow it's parent.
+ */
+const calcDelta = (
+  origin: Position2D,
+  position: Position2D,
+  bounds?: Rect,
+  size?: Rect
+): Position2D => {
+  // If no size is given, use a non-sized rect instead.
+  const cleanSize = size || emptyBoundingRect();
+
+  /**
+   * These min/max fields define the minimum and maximum values which we're allowed to move the element in.
+   * It uses the size of the element to make sure the element doesn't overflow the parent.
+   */
+  const maxX = bounds
+    ? bounds.left +
+      bounds.width -
+      (cleanSize.width - (origin.x - cleanSize.left))
+    : Infinity;
+
+  const maxY = bounds
+    ? bounds.top +
+      bounds.height -
+      (cleanSize.height - (origin.y - cleanSize.top))
+    : Infinity;
+
+  const minX = bounds ? bounds.left + (origin.x - cleanSize.left) : -Infinity;
+
+  const minY = bounds ? bounds.top + (origin.y - cleanSize.top) : -Infinity;
+
+  const x = Math.max(Math.min(position.x, maxX), minX);
+  const y = Math.max(Math.min(position.y, maxY), minY);
+
+  // Subtract it from the origin so we know the delta.
+  const xDiff = x - origin.x;
+  const yDiff = y - origin.y;
+  return { x: xDiff, y: yDiff };
+};
+
+type FromMoveArgs = {
+  /**
+   * The current event.
+   */
+  position: Position2D;
+  /**
+   * The original event from which we started dragging.
+   */
+  origin: Position2D;
+  /**
+   * The bounds in which we're allowed to move.
+   */
+  bounds?: BoundingRect;
+  /**
+   * The size of the element being moved.
+   */
+  elementSize: BoundingRect;
+};
+
+const fromMove = ({
+  position,
+  origin,
+  bounds,
+  elementSize,
+}: FromMoveArgs): MoveData => {
+  const delta = calcDelta(origin, position, bounds, elementSize);
+
+  const rect = {
+    ...elementSize,
+    left: elementSize.left + delta.x,
+    top: elementSize.top + delta.y,
+  };
+
+  return {
+    rect,
+    delta,
+    origin,
+  };
+};
+
+/**
+ * Default measuring method, used to measure the size of the thing we're moving.
+ * @param e The element we're trying to measure
+ */
+const defaultMeasure = (e: HTMLElement) => {
+  return e.getBoundingClientRect();
+};
+
+/**
+ * Supports mouse/touch events out of the box
+ */
+const defaultConnector = withMouse(withTouch());
+
+/**
+ * React Hook that allows an element to be moved around. Returns a callback.
+ *
+ * Takes onMove, onMoveStart, onMove callback properties.
+ *
+ * @param props Props of the hook.
+ */
+export const useMovable = (props: UseMovableProps) => {
+  /**
+   * Reference to the element we're moving.
+   */
+  const ref = useRef<HTMLElement>();
+
+  // The first event from which we started the current movement.
+  const origin = useRef<Position2D>();
+  /**
+   * This is the original bounding rect of the element we started moving.
+   * Used by calcDelta to to make sure we don't overflow the parent element.
+   */
+  const originalBoundingRect = useRef<BoundingRect>();
+  const lastMove = useRef<Position2D>();
+
+  const {
+    bounds: boundsProp,
+    connector = defaultConnector,
+    measure: measureProp = defaultMeasure,
+    unbounded,
+    onMove,
+    onMoveStart,
+    onMoveEnd,
+    sizeRef,
+  } = props;
+
+  const measure = useCallback(
+    (el: HTMLElement) => {
+      return toBoundingRect(measureProp(el));
+    },
+    [measureProp]
+  );
+
+  /**
+   * Either returns a BoundingRect in which the element is allowed to be moved. Or returns undefined,
+   * meaning the object can be moved unbounded. If no bounds was given in UseMovableProps, it uses
+   * the parent of the element as its bounds.
+   */
+  const getBounds = useCallback((): BoundingRect | undefined => {
+    if (boundsProp || !unbounded) {
+      if (isBoundingRect(boundsProp)) return boundsProp;
+      if (typeof boundsProp === "function") {
+        const bounds = boundsProp();
+        return isBoundingRect(bounds) ? bounds : undefined;
+      }
+      if (boundsProp?.current) {
+        return boundsProp.current.getBoundingClientRect();
+      }
+
+      if (sizeRef?.current && sizeRef?.current.parentElement) {
+        return measure(sizeRef?.current.parentElement);
+      }
+
+      if (ref?.current?.parentElement) {
+        return measure(ref?.current?.parentElement);
+      }
+    }
+    return undefined;
+  }, [boundsProp, unbounded, sizeRef, measure]);
+
+  // This starts a movement. Also sets the origin and original size of the movable element.
+  const moveStart = useCallback(
+    (ev: Position2D = emptyPosition2D()) => {
+      origin.current = ev;
+      lastMove.current = ev;
+      if (ref.current) {
+        originalBoundingRect.current = measure(sizeRef?.current || ref.current);
+        if (onMoveStart) {
+          onMoveStart(
+            fromMove({
+              origin: ev,
+              position: ev,
+              bounds: getBounds(),
+              elementSize: originalBoundingRect.current,
+            })
+          );
+        }
+      }
+    },
+    [getBounds, onMoveStart, sizeRef]
+  );
+
+  // Called when the element is moving.
+  const moveTo = useCallback(
+    (position: Position2D) => {
+      if (origin.current && originalBoundingRect.current) {
+        const moveData = fromMove({
+          position,
+          origin: origin.current,
+          bounds: getBounds(),
+          elementSize: originalBoundingRect.current,
+        });
+        lastMove.current = position;
+        if (onMove) onMove(moveData);
+      }
+    },
+    [getBounds, onMove]
+  );
+
+  const move = useCallback((movement: Partial<Position2D>) => {
+    if (origin.current && originalBoundingRect.current && lastMove.current) {
+      const base = lastMove.current;
+      const position = {
+        x: base.x + (movement.x || 0),
+        y: base.y + (movement.y || 0),
+      };
+      const moveData = fromMove({
+        position,
+        origin: origin.current,
+        bounds: getBounds(),
+        elementSize: originalBoundingRect.current,
+      });
+      lastMove.current = moveData.delta;
+      if (onMove) onMove(moveData);
+    }
+  }, []);
+
+  // Called when the element finished moving.
+  const moveEnd = useCallback(() => {
+    if (origin.current && lastMove.current && originalBoundingRect.current) {
+      if (onMoveEnd) {
+        onMoveEnd(
+          fromMove({
+            position: lastMove.current,
+            origin: origin.current,
+            bounds: getBounds(),
+            elementSize: originalBoundingRect.current,
+          })
+        );
+      }
+      lastMove.current = undefined;
+      origin.current = undefined;
+    }
+  }, [getBounds, onMoveEnd]);
+
+  // Attaches mousedown listen to the given element. If there's still an old one, remove the listeners on the old one first.
+  const callback = useCallback(
+    (node: HTMLElement | null) => {
+      if (!node) return;
+      if (ref.current) connector.disconnect(ref.current);
+      ref.current = node;
+      connector.connect(node, {
+        move,
+        moveTo,
+        moveStart,
+        moveEnd,
+      });
+    },
+    [connector, move, moveTo, moveEnd, moveStart]
+  );
+
+  useEffect(() => {
+    return () => {
+      if (ref.current) connector.disconnect(ref.current);
+    };
+  }, []);
+
+  return callback;
+};
