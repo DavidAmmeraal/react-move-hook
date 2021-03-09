823d7f9cd33464c999815197530f9803d6cd2548
David Ammeraal <david.ammeraal@gmail.com>
2021-03-09 23:31:15 +0100

    Updated readme.

--- examples/basic/src/App.js
+++ examples/basic/src/App.js
@@ -1,32 +1,36 @@
 import React, { useState } from "react";
 import { useMovable } from "react-move-hook";
 
+const containerStyle = {
+  position: "absolute",
+  margin: "10px",
+  width: "300px",
+  height: "300px",
+  backgroundColor: "lightgray",
+};
+
+const movableStyle = {
+  position: "absolute",
+  padding: "5px",
+  top: 0,
+  left: 0,
+  border: "1px solid black",
+};
+
 function App() {
   const [move, setMove] = useState();
 
   const ref = useMovable({ onMove: setMove });
 
   return (
-    <div
-      style={{
-        position: "absolute",
-        margin: "10px",
-        width: "300px",
-        height: "300px",
-        backgroundColor: "lightgray",
-      }}
-    >
+    <div style={containerStyle}>
       <div
         ref={ref}
         style={{
-          position: "absolute",
-          padding: "5px",
+          ...movableStyle,
           transform: `translate3d(${move?.accDelta.x || 0}px, ${
             move?.accDelta.y || 0
           }px, 0)`,
-          top: 0,
-          left: 0,
-          border: "1px solid black",
         }}
       >
         MOVE ME
